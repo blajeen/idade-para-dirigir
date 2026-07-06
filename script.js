361367df7b3fe86f75d4d2e5ddeb116521e6fe5c
blajeen <brg.ftw@gmail.com>
2026-07-06 19:48:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ alert('Bem-vindo ao nosso site!');
 let nome = prompt("Qual seu nome?");
 let idade = Number(prompt("Qual sua idade?"));
 
-while (idade < 18){
 if (idade >= 18) {
     alert(`${nome}, você pode tirar habilitação!`);
 } else {
     alert(`${nome}, você ainda não tem idade para dirigir!`);
 }
-idade = Number(prompt("que pena tente novamente quando tiver mais velho")
-               }
